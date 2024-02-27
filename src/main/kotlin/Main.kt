2c44c2883bc7f2c7b4e6757d39a71b760dc5c809
Tuukuo <158445207+Tuukuo@users.noreply.github.com>
2024-02-27 08:47:49 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 fun main(){
     println(workersSalary(12, 2300))
+    println(workersSalary(6,1000))
+    println(workersSalary(4,500))
 }
 fun workersSalary(hours :Int, rate : Int ) : Int{
     return hours * rate
